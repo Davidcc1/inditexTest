7015942604982c25a1a8d92fa4b2284115a25866
David <davidcc72@gmail.com>
2025-05-26 19:52:20 +0200

    Security implemented

--- src/test/java/com/inditex/test/controller/PricingControllerTest.java
+++ src/test/java/com/inditex/test/controller/PricingControllerTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -18,6 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PricingController.class)
+@Import(SecurityException.class)
 class PricingControllerTest {
 
     @Autowired
@@ -38,7 +41,17 @@ class PricingControllerTest {
         mockMvc.perform(get("/api/pricing")
                 .param("requestedDate", formatter.format(LocalDateTime.now(fixed)))
                 .param("productId", "1119")
-                .param("brandId", "1"))
+                .param("brandId", "1")
+                        .header("Authorization", "Basic " + Base64.getEncoder().encodeToString("admin:admin".getBytes())))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnUnauthorizedWhenNoAuthenticationProvided() throws Exception {
+        mockMvc.perform(get("/api/pricing")
+                        .param("productId", "35455")
+                        .param("brandId", "1")
+                        .param("requestedDate", "2020-06-14T10:00:00"))
+                .andExpect(status().isUnauthorized());
     }
 }
